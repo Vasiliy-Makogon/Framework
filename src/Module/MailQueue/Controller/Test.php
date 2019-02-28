fcae979b75ab524264c4d28d1fe41f1811525eaa
Василий Макогон (U1735607) <vasiliy.makogon@crediteurope.ru>
2019-02-28 14:18:21 +0300

    anchor

--- src/Module/MailQueue/Controller/Test.php
+++ src/Module/MailQueue/Controller/Test.php
@@ -18,7 +18,7 @@ class Test extends Controller
         $mailQueue = new MailQueue();
         $mailQueue
             ->setSendDate(new \Krugozor\Framework\Type\Datetime())
-            ->setTemplate(Application::getAnchor('MailQueue')::getPath() . '/Template/Test.mail')
+            ->setTemplate(Application::getAnchor('MailQueue')::getPath('/Template/Test.mail'))
             ->setToEmail('to_email@test.ru')
             ->setFromEmail('from_email@test.ru')
             ->setHeader('test mail')
