f2d4d85e80ab4362d72280a16d16d2e9db4fa6a4
Василий Макогон (U1735607) <vasiliy.makogon@crediteurope.ru>
2019-02-28 11:42:14 +0300

    anchor

--- src/Registry.php
+++ src/Registry.php
@@ -21,13 +21,12 @@ class Registry extends CoverArray implements Singleton
     {
         if (self::$instance === null) {
             if ($config_file_path === null || !file_exists($config_file_path)) {
-                throw new \RuntimeException('Не найден файл конфигурации ' . $config_file_path);
+                throw new \RuntimeException("Configuration file not found at path `$config_file_path`");
             }
 
             $config = parse_ini_file($config_file_path, true);
-
             if (!$config) {
-                throw new \RuntimeException('Невозможно прочитать файл конфигурации ' . $config_file_path);
+                throw new \RuntimeException("Unable to read configuration file by path `$config_file_path`");
             }
 
             self::$instance = new self($config);
